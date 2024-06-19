20d22933d6ed943f0062c43f37b3017be84f56ba
Pe4EHKA <Pe4EHKA.COM@yandex.ru>
2024-06-19 13:28:25 +0500

    Initial commit

---
+++ src/main/java/ufanet/practika/fitness_telegram_bot/FitnessTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ufanet.practika.fitness_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufanet/practika/fitness_telegram_bot/FitnessTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ufanet.practika.fitness_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
